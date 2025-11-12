049e5c98c4c9832f04a03da2e16f4e8ce2389453
Muhammad Nadeem Akbar <>
2025-11-12 19:54:52 +0500

    Added User API endpoints

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,22 +2,24 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 
 class AuthController extends Controller
 {
-    // for user register 
+    // for user register -------------------------------
     public function signup(Request $request)
     {
         $Validateuser = Validator::make(
             $request->all(),
             [
-                'name' => 'required',
-                'email' => 'required|email|unique:users,email',
-                'password' => 'required',
+                'name' => 'required||string|min:3|max:50|regex:/^[a-zA-Z\s]+$/',
+                'email' => 'required||string|max:255|email|unique:users,email',
+                'password' => 'required|max:255|confirmed',
 
             ]
         );
@@ -33,7 +35,7 @@ class AuthController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-           'password' => $request->password,
+            'password' => bcrypt($request->password),
         ]);
         return response()->json([
             'status' => true,
@@ -42,33 +44,45 @@ class AuthController extends Controller
         ], 200);
     }
 
-    // for login user 
-    public function login()
+    // for login user ----------------------------------------
+    public function login(Request $request)
     {
-        $userValidate = Validator::make([
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
+        try {
+             $userValidate = Validator::make(
+            $request->all(),
+            [
+                'email' => 'required|string|max:255|email',
+                'password' => 'required|max:255',
+            ]
+        );
 
-        if ($userValidate->fails()) {
+         // Auth attempt 
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $Authuser = Auth::user();
+            return response()->json([
+                'status' => true,
+                'message' => "User login successfully",
+                'token' => $Authuser->createToken('auth_token')->plainTextToken,
+                'token_type' => 'bearer',
+            ], 200);
+        }else {
+            return response()->json([
+                'status' => false,
+                'message' => "Invalid Password",
+            ], 401);
+        }
+
+        } catch (\Throwable $th) {
+            //throw $th;
+             if ($userValidate->fails()) {
             return response()->json([
                 'status' => false,
                 'message' => 'User are un athenciate',
                 'errors' => $userValidate->errors()->all(),
             ], 404);
         }
-        // Auth attempt 
-        if(Auth::attempt(['email' => $request->email, 'password', $request->password])){
-            $Authuser = Auth::user();
-            return response()->json([
-                'status' => true,
-                'message' => "User login successfully",
-                'token' => $Authuser->createToken('auth_token')->plainTextToken,
-                'tpekn_type' => 'bearer',
 
-            ],200);
-        } else {
-            return response()->json([
+             return response()->json([
                 'status' => false,
                 'message' => 'Email password does not match',
                 'errors' => $userValidate->errors()->all(),
@@ -76,15 +90,27 @@ class AuthController extends Controller
         }
     }
 
-    // for logout 
-    public function logout(Request $request) {
-        $request->user();
-        $request->tokens()->delete();
-        return response()->json([
-            'status' => true,
-            'message' => 'Logout succesfully',
+    // for logout ----------------------------------------------
+    public function logout(Request $request)
+    {
+        try {
+            $request->user()->tokens()->delete();
+            Log::info("User Logout Successfully", ['user_id' => $request->user()->id]);
+            return response()->json([
+                'status' => true,
+                'message' => 'Logout successfully',
+            ], 200);
+
+            
+        } catch (\Throwable $th) {
+            Log::error('Error during logout', [
+            'message' => $th->getMessage(),
         ]);
+            return response()->json([
+                'status' => false,
+                'message' => "Some thing went worong", 
 
+            ],500);
+        }
     }
-
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+     protected $fillable = [
+        'user_id',
+        'task_name',
+        'description',
+        'image',
+        'created_at',
+        'status',
+        'is_deleted',
+        'updated_at',
+
+
+    ];
 }
